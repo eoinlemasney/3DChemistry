df6f3a3f82353d5fe0e0a1202e99c1bec9032c11
eoinlemasney <35727679+eoinlemasney@users.noreply.github.com>
2021-05-21 22:55:32 +0100

    Organising folders

---
+++ Assets/Scripts/FX/Destructable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destructable : MonoBehaviour
+{
+    //Get the reference of the destroyed prefab
+    public GameObject destroyedVersion;
+    void OnMouseDown() {
+        // When the user clicks the object
+        //Instantiate the 'cracked' version and destroy the original object
+        Debug.Log("Destroy object here");
+        Instantiate(destroyedVersion, transform.position, transform.rotation);
+        Destroy(gameObject);
+
+    }
+}
