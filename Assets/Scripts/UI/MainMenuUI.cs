67252762148963e4cf8f0c162d8a69bec5cd91a6
eoinlemasney <35727679+eoinlemasney@users.noreply.github.com>
2021-05-23 14:30:16 +0100

    Adding in the Main Menu

---
+++ Assets/Scripts/UI/MainMenuUI.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuUI : MonoBehaviour
+{
+    public string helpUrl = "https://www.theconicalflask.ie/j-c-science/chemistry";
+    public void PlayGame()
+    {
+        //Loads the second scene in the game
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    // Update is called once per frame
+    public void QuitGame()
+    {
+        Application.Quit();
+        
+    }
+
+    public void OpenLink() {
+        Application.OpenURL(helpUrl);
+    }
+}
